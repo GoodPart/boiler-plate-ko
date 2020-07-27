80f2a35dfccc81d7a292115455d9f18fca3ccdba
GoodPart <pks940122@cttd.co.kr>
2020-07-28 02:33:07 +0900

    리덕스 추가 및 로그인까지 완료 복습 필요

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,31 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import LandingPage from './components/views/LandingPage/LandingPage'
-import LoginPage from './components/views/LoginPage/LoginPage'
-import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+import "./App.css";
 
 function App() {
   return (
-  <Router>
-        <div>
-
-          {/*
+    <Router>
+      <div>
+        {/*
             A <Switch> looks through all its children <Route>
             elements and renders the first one whose path
             matches the current URL. Use a <Switch> any time
             you have multiple routes, but you want only one
             of them to render at a time
           */}
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/LoginPage" component={LoginPage} />
-            <Route path="/RegisterPage" component={RegisterPage}/>
-          </Switch>
-        </div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/LoginPage" component={LoginPage} />
+          <Route path="/RegisterPage" component={RegisterPage} />
+        </Switch>
+      </div>
     </Router>
   );
 }
 
-
-
 export default App;
-
