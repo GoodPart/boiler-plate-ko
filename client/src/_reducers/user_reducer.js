bd24656818228d2f736f8dbba0953b7c52dba5b1
GoodPart <40373934+GoodPart@users.noreply.github.com>
2020-07-28 17:57:47 +0900

    Redux 주석 작성

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -3,7 +3,12 @@ import { LOGIN_USER } from "../_action/types";
 export default function (state = {}, action) {
   switch (action.type) {
     case LOGIN_USER:
-      return { ...state, loginSuccess: action.payload };
+
+    //여기서 loginSuccess부분은 하나의 key : value(user_action에서 받은 값)을 의미
+    //기존에 state값을 스프레드 시트를 이용해 불변성을 유지한 채 복사해온다.
+    //여기서는 state값이 변경된게 없으니 리턴할게 없음
+      return { ...state, payloadValue: action.payload };
+      console.log('123123',state)
       break;
 
     default:
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -17,18 +17,27 @@ function LoginPage(props) {
     setPassword(event.currentTarget.value);
   };
 
+  //제출 이벤트 실행시 클라이언트의 값을 서버로 보내야한다.
+  //여기서 Axios를 사용해서 보내면 되지만, Redux를 사용함.
+  //상단에 dispatch를 선언하고 사용함.
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    console.log("email", Email);
-    console.log("Password", Password);
+    // console.log("email", Email);
+    // console.log("Password", Password);
 
+    //state값 변경 발생 -> dispatch -> action -> reducer ->component가 렌더링댐
+
+    // state값을 body에 변수 body에 담아 dispatch(액션을 발생시키는 단계)
     let body = {
       email: Email,
       password: Password,
     };
 
+    //변경될 state값을 body에 태워 lifeCycle을 모두 통과하면 .then(...)이하가 실행된다.**** _action 폴더로 이동하기 ****
     dispatch(loginUser(body)).then((response) => {
+      //서버에서 받은 값의 loginSuccess(server/index.js에 코딩해둠)가true면 
       if (response.payload.loginSuccess) {
+        // push('/')페이지로 이동시켜라
         props.history.push("/");
       } else {
         console.log("err");
